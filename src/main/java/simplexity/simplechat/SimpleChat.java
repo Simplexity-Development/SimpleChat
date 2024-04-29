375d5dcd4a9d5c8d847a57b614dbcf2d6a8c1ba4
Rhythmic System <mc.adhdmc@gmail.com>
2024-04-29 14:44:59 -0700

    Changed to Maven, changed PAPI dependency to soft dependency

--- src/main/java/simplexity/simplechat/SimpleChat.java
+++ src/main/java/simplexity/simplechat/SimpleChat.java
@@ -6,6 +6,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import simplexity.simplechat.commands.SimpleChatReload;
 import simplexity.simplechat.listeners.AsyncChatListener;
+import simplexity.simplechat.listeners.AsyncChatListenerPAPI;
 import simplexity.simplechat.utils.Message;
 
 import java.util.Objects;
@@ -20,10 +21,9 @@ public final class SimpleChat extends JavaPlugin {
     public void onEnable() {
         instance = this;
         if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
-            Bukkit.getPluginManager().registerEvents(new AsyncChatListener(), this);
+            Bukkit.getPluginManager().registerEvents(new AsyncChatListenerPAPI(), this);
         } else {
-            getLogger().warning("SimpleChat requires PlaceholderAPI to be installed. Disabling SimpleChat.");
-            Bukkit.getPluginManager().disablePlugin(this);
+            Bukkit.getPluginManager().registerEvents(new AsyncChatListener(), this);
         }
         Objects.requireNonNull(this.getCommand("simplechatreload")).setExecutor(new SimpleChatReload());
         this.saveDefaultConfig();
--- src/main/java/simplexity/simplechat/listeners/AsyncChatListenerPAPI.java
+++ src/main/java/simplexity/simplechat/listeners/AsyncChatListenerPAPI.java
@@ -1,20 +1,24 @@
 package simplexity.simplechat.listeners;
 
 import io.papermc.paper.event.player.AsyncChatEvent;
+import me.clip.placeholderapi.PlaceholderAPI;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.minimessage.tag.Tag;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.jetbrains.annotations.NotNull;
 import simplexity.simplechat.SimpleChat;
 import simplexity.simplechat.utils.ChatPermission;
 import simplexity.simplechat.utils.Message;
 
-public class AsyncChatListener implements Listener {
+public class AsyncChatListenerPAPI implements Listener {
     
     MiniMessage miniMessage = SimpleChat.getMiniMessage();
     
@@ -26,6 +30,7 @@ public class AsyncChatListener implements Listener {
         chatEvent.message(messageParsed);
         chatEvent.renderer((source, sourceDisplayName, chatMessage, viewer) ->
                 miniMessage.deserialize(Message.CHAT_FORMAT.getMessage(),
+                        papiTag(source),
                         Placeholder.component("player", sourceDisplayName),
                         Placeholder.component("message", messageParsed)));
     }
@@ -42,6 +47,28 @@ public class AsyncChatListener implements Listener {
         MiniMessage msgParser = MiniMessage.builder().tags(tagResolver.build()).build();
         return msgParser.deserialize(message);
     }
-
+    
+    //Credit: https://docs.advntr.dev/faq.html#how-can-i-use-bukkits-placeholderapi-in-minimessage-messages
+    
+    /**
+     * Creates a tag resolver capable of resolving PlaceholderAPI tags for a given player.
+     *
+     * @param player the player
+     *
+     * @return the tag resolver
+     */
+    public @NotNull TagResolver papiTag(final @NotNull Player player) {
+        return TagResolver.resolver("papi", (argumentQueue, context) -> {
+            final String papiPlaceholder = argumentQueue.popOr(Message.ERROR_PAPI_NEEDS_ARGUMENT.getMessage()).value();
+            final String parsedPlaceholder = PlaceholderAPI.setPlaceholders(player, '%' + papiPlaceholder + '%');
+            Component componentPlaceholder;
+            if (parsedPlaceholder.contains("§")) {
+                componentPlaceholder = LegacyComponentSerializer.legacySection().deserialize(parsedPlaceholder);
+            } else {
+                componentPlaceholder = miniMessage.deserialize(parsedPlaceholder);
+            }
+            return Tag.selfClosingInserting(componentPlaceholder);
+        });
+    }
     
 }
